25bd9d7d8cd2ec978743371ee1293be5c36d7385
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-29 03:12:58 +0000

    Deploy apps.vechain.org to github.com/vechain/app-hub-app.git:gh-pages

---
+++ precache-manifest.dac2d7051211bc8bb0a923e2d5e5ea02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7993dbe29d806df3f97",
+    "url": "/css/app.0a3da92b.css"
+  },
+  {
+    "revision": "e7993dbe29d806df3f97",
+    "url": "/js/app.ccdc05a3.js"
+  },
+  {
+    "revision": "2c1c5089e834ac9e1e79",
+    "url": "/css/chunk-vendors.fa7d0e5c.css"
+  },
+  {
+    "revision": "2c1c5089e834ac9e1e79",
+    "url": "/js/chunk-vendors.db081ecd.js"
+  },
+  {
+    "revision": "3bf81dcbe05872c604d80802a6197a68",
+    "url": "/img/com.vechain.inspector.3bf81dcb.png"
+  },
+  {
+    "revision": "6908579b7ce954b378f3772b83bcecbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
